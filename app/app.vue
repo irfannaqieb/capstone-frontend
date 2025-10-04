3913a0d15a3949a473cfc8af33a8053f935d566b
Irfan Baharuddin <irfannaqieb@gmail.com>
2025-10-04 12:54:49 +0900

    initial setup. includes tailwind, shadcn, vueuse core...

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="mx-auto flex min-h-screen w-full max-w-3xl flex-col items-center justify-center p-4"
+  >
+    <Button>Button</Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+</script>
